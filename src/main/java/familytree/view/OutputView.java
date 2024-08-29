a3988e1f3d61dbb7ff03b158c45d65ab8ae0607b
FaneSl <crf.cool@bk.ru>
2024-08-29 19:00:55 +0300

    Домашняя работу по уроку 6 (ооп)
    Signed-off-by: FaneSl <crf.cool@bk.ru>

--- src/main/java/familytree/view/OutputView.java
+++ src/main/java/familytree/view/OutputView.java
@@ -3,10 +3,8 @@ package familytree.view;
 import familytree.model.Person;
 import java.util.List;
 
-public interface FamilyTreeView {
+public interface OutputView {
     void displayMenu();
-    int getMenuChoice();
-    String getInput(String prompt);
     void displayMessage(String message);
     void displayPerson(Person person);
     void displayPersonList(List<Person> people);
